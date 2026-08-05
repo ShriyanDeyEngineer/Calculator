ec6cd8f8a50bf81ce5c2ce6f13cdda8f8ff8545a
ShriyanDeyEngineer <shriyandey07@gmail.com>
2026-08-04 19:54:09 -0500

    SUCCESSFULLY IMPLEMENT PRIMARY FEATURE

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//variable declarations
+let memory = new Array(3);
+let screenContent = document.querySelector("#calculator-screen");
+
+
 //functions
 function operate(operator, num1, num2)
 {
@@ -21,17 +26,84 @@ function subtract(num1, num2){return num1 - num2;}
 
 function multiply(num1, num2){return num1 * num2;}
 
-function divide(num1, num2){return num1 / num2;}
+function divide(num1, num2){return (num2 == 0) ? "ERROR" : num1 / num2;}
 
 
 //Button functionality
-/*const numberButtons = Array.from(document.querySelectorAll(".number-button"));
+document.querySelector("#clear-button").addEventListener('click', event => {
+    screenContent.innerText = "";
+    memory.length = 0;
+    memory.length = 3;
+});
 
-numberButtons.forEach(numberButton => {addEventListener('click', event => {
-    document.querySelector("#calculator-screen").innerText = numberButton.innerText;
-    console.log(numberButton.innerText);
-});});*/
 document.querySelectorAll(".number-button").forEach(numberButton => 
     numberButton.addEventListener('click', event => {
-    document.querySelector("#calculator-screen").innerText = event.target.innerText;
+        //case where there is a digit in the first spot of memory and not an operator in memory or there are no digits 
+        //in the first spot of the memory
+        if((typeof(memory[0]) == "string" && typeof(memory[1]) != "string") || typeof(memory[0]) != "string")
+        {
+            screenContent.innerText += event.target.innerText;
+
+            typeof(memory[0]) != "string" ? memory[0] = event.target.innerText : memory[0] += event.target.innerText;
+
+            console.log(memory);
+        }
+        //case where there is an operator in the memory
+        else if(typeof(memory[1] == "string"))
+        {
+            if(typeof(memory[2]) != "string")
+            {
+                screenContent.innerText = "";
+                screenContent.innerText += event.target.innerText;
+                memory[2] = event.target.innerText;
+            }
+            else
+            {
+                screenContent.innerText += event.target.innerText;
+                memory[2] += event.target.innerText;
+            }
+
+            console.log(memory);
+        }
 }));
+
+document.querySelectorAll(".operator-button").forEach(operatorButton => 
+    operatorButton.addEventListener('click', event => {
+        //case where num1 is in memory and an operator is ready to be included as well
+        //using memory[2] in case user wants to change the selected operator before finishing the rest of the operation
+        if(typeof(memory[0]) == "string" && typeof(memory[2]) != "string"){memory[1] = event.target.innerText;}
+
+        //case where memory is filled with the three items needed for an operation 
+        //(operator now acts as equal sign then replaces the previous operator in memory)
+        else if(typeof(memory[2] == "string"))
+        {
+            screenContent.innerText = operate(memory[1], memory[0], memory[2]);
+            memory.length = 0;
+            memory.length = 3;
+            memory[0] = screenContent.innerText;
+            memory[1] = event.target.innerText;
+        }
+
+    /*if(event.target.style.backgroundColor != "darkgray")
+    {
+        event.target.style.backgroundColor = "darkgray";
+        console.log("clicked");
+    }
+    else if(event.target.style.backgroundColor === "darkgray")
+    {
+        event.target.style.backgroundColor = "";
+        console.log("clicked2");
+    }*/ //a code block to be used to show the user the currently selected button
+        console.log(memory);
+}));
+
+document.querySelector("#equals-button").addEventListener('click', event => {
+    if(typeof(memory[2]) == "string")
+    {
+        screenContent.innerText = operate(memory[1], memory[0], memory[2]);
+        memory.length = 0;
+        memory.length = 3;
+        memory[0] = screenContent.innerText;
+        memory[1] = event.target.innerText;
+    }
+});
